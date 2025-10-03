08742be3f244ee621b0b095203b5a1bbc30dbcee
MuhamadFaridB25 <muhamadfaridalmubarok10012007@gmail.com>
2025-10-03 15:13:02 +0800

    Percabangan

---
+++ kelas/pertemuan-4/percabangan.py
@@ -0,0 +1,38 @@
+# for i in range (1, 11, 2):
+    # print(f'Perulangan ke {i}')
+
+#mahasiswa = []
+
+#for a in mahasiswa:
+    #print(a) 
+
+#for i in range(1, 10):
+    #for j in range(1, 3):
+ #       print(f'{i} x {j} = {i * j}')
+    #print('')
+
+#for i in range (1, 10):
+
+
+
+
+#while(jawab == 'ya'):
+##  jawab = inZZput("Ulang lagi? ")
+#print(f"Total perulangan: {hitung}")
+
+while True:
+    print('MENU')
+    print('1. HP')
+    print('2. LAPTOP')
+    print('3. HEADSET')
+    opsi = int(input('Masukkan opsi: '))
+    if opsi == 1:
+        print('1. HP')
+    elif opsi == 2:
+        print('2. LAPTOP')
+    elif opsi == 3:
+        print('3. HEADSET')
+    else:
+        print('Pilihan Tidak Valid')
+        break
+    
