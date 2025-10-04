e348a90da674717ae15059daeb4b0b2cc942c725
MuhamadFaridB25 <muhamadfaridalmubarok10012007@gmail.com>
2025-10-04 19:33:13 +0800

    Update

---
+++ post-test/post-test-apd-4/2509106087-MuhamadFaridAlMubarok-PT-4.py
@@ -0,0 +1,22 @@
+# Validasu Login
+
+username = "Muhamad Farid Al Mubarok"
+password = "2509106087"
+maksimal_percobaan = 3
+percobaan = 0
+
+print()
+print("=== Username dan Password untuk Melanjutkan Pembelian Furnitur ===")
+print()
+usernameinput = input("Masukkan Username: ")
+passwordinput = input("Masukkan Password: ")
+
+while percobaan < maksimal_percobaan:
+    print()
+    print("=== Username dan Password untuk Melanjutkan Pembelian Furnitur ===")
+    print()
+    usernameinput = input("Masukkan Username: ")
+    passwordinput = input("Masukkan Password: ")
+    
+
+
