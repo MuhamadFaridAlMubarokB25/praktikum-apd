a554870db21452d90386ef862b68b56ee57d6b5e
MuhamadFaridB25 <muhamadfaridalmubarok10012007@gmail.com>
2025-11-04 22:28:35 +0800

    The End Of PostTest APD

--- kelas/pertemuan-2/pertemuan-apd-2.py
+++ kelas/pertemuan-2/pertemuan-apd-2.py
@@ -1,4 +1,81 @@
-nama = "Muhamad Farid Al Mubarok"
-NIM = "2509106087"
+from prettytable import PrettyTable
+import inquirer
+import os
+os.system('cls')
 
-namainput = input
+def menu_utama():
+    while True:
+        menu_utama = PrettyTable()
+        menu_utama.field_names = ["No", "Pilihan Menu"]
+        menu_utama.align = "l"
+        menu_utama.hrules = 1
+        menu_utama.add_row(['1', 'CUSTOMER'])
+        menu_utama.add_row(['2', 'ADMIN'])
+        menu_utama.add_row(['3', 'KELUAR'])
+        os.system
+        print('=== SELAMAT DATANG DI TOKO PARFUM X ===')
+        print('Anda Login Sebagai Siapa?')
+        print(menu_utama)
+
+        pertanyaan = [
+        inquirer.List(
+            "pilihan",
+            message="Pilih menu",
+            choices=[("1. Customer", 1), 
+                    ("2. Admin", 2), 
+                    ("3. Keluar", 3)]
+        )
+        ]
+
+        answers = inquirer.prompt(pertanyaan)
+
+        if answers["pilihan"] == 1:
+            print('12')
+        elif answers["pilihan"] == 2:
+            print('32')
+
+        input('ENTER')
+
+
+menu_utama()
+
+
+pertanyaan = [
+inquirer.List(
+    "pilihan",
+    message="Silahkan Pilih Opsi Login",
+    choices=[("1. Customer", 1), 
+            ("2. Admin", 2), 
+            ("3. Keluar", 3)]
+)
+]
+answers = inquirer.prompt(pertanyaan)
+
+if answers["pilihan"] == 1:
+    menu_cust1()
+elif answers["pilihan"] == 2:
+    menu_admin1()
+else:
+    answers["pilihan"] == 3
+    print('\nTerima Kasih Sudah Berkunjung :)')
+    exit()
+        # try:
+        #     opsilogin = int(input('Masukkan Pilihan (1-3): '))
+        #     input('\nTekan Enter Untuk Melanjutkan...')
+
+        #     if opsilogin == 1:
+        #         menu_cust1()
+        #     elif opsilogin == 2:
+        #         menu_admin1()
+        #     elif opsilogin == 3:
+        #         print('\nTerima Kasih Sudah Berkunjung :)')
+        #         exit()
+        #     else:
+        #         print('\nPilihan Tidak Valid! Silahkan Pilih Angka 1-3')
+        #         input('\nTekan Enter Untuk Kembali...')
+        #         continue
+        # except ValueError:
+        #     print('\nPilihan Harus Berupa Angka! Silahkan Pilih Angka 1-3')
+        #     input('\nTekan Enter Untuk Kembali...')
+        #     continue
+
---
+++ post-test/post-test-apd-8/2509106087_MuhamadFaridAlMubarok-PT-8/login.py
@@ -0,0 +1,38 @@
+from registrasi import akuns, akunadmin
+from customer2 import menu_cust2
+from admin2 import menu_admin2
+import pwinput
+
+def login_cust():
+    print('\nHalo Customer!, Silahkan Login Terlebih Dahulu.')
+
+    usernamecust = input('Masukkan Username: ')
+    passwordcust = pwinput.pwinput(prompt="Masukkan Password: ", mask="*")
+
+    if len(akuns) == 0:
+        print('\nAnda Belum Memiliki Akun. Silahkan Buat Akun Untuk Login!')
+        input('\nTekan Enter Untuk Kembali...')
+    elif usernamecust in akuns and akuns[usernamecust] == passwordcust:
+        print('\nLogin Berhasil!')
+        input('\nTekan Enter Untuk Melanjutkan...')
+        menu_cust2()
+    else:
+        print('\nUsername atau Password Salah. Silahkan Coba Lagi')
+        input('\nTekan Enter Untuk Kembali...')
+
+def login_admin():
+    print('\nHalo Admin!, Silahkan Login Terlebih Dahulu.')
+
+    usernameadmin = input('Masukkan Username: ')
+    passwordadmin = pwinput.pwinput(prompt="Masukkan Password: ", mask="*")
+
+    if len(akunadmin) == 0:
+        print('\nAnda Belum Memiliki Akun. Silahkan Buat Akun Untuk Login!')
+        input('\nTekan Enter Untuk Kembali...')
+    elif usernameadmin in akunadmin and akunadmin[usernameadmin] == passwordadmin:
+        print('\nLogin Berhasil!')
+        input('\nTekan Enter Untuk Melanjutkan...')
+        menu_admin2()
+    else:
+        print('\nUsername atau Password Salah. Silahkan Coba Lagi')
+        input('\nTekan Enter Untuk Kembali...')
